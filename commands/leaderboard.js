58970b5abc3ac9f372b6fb6eb102bab211a0ba04
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-01 20:44:54 -0800

    Update leaderboard.js

--- commands/leaderboard.js
+++ commands/leaderboard.js
@@ -12,10 +12,10 @@ module.exports = {
         }
 
         keys.sort((firstEl, secondEl) => { 
-            if (userData[firstEl].points < userData[secondEl].points) {
+            if (userData[firstEl].points > userData[secondEl].points) {
                 return -1;
             }
-            if (userData[firstEl].points > userData[secondEl].points) {
+            if (userData[firstEl].points < userData[secondEl].points) {
                 return 1;
             }
             return 0;
@@ -26,8 +26,10 @@ module.exports = {
         var names = "";
         var points = "";
         for (var k in keys) {
-            names += userData[k].name + "\n";
-            points += userData[k].points + "\n";
+            if (userData[k]) {
+                names += userData[k].name + "\n";
+                points += userData[k].points + "\n";
+            }
         }
         embedMsg.setTitle("**__The Leaderboard__**");
         embedMsg.setFields(
