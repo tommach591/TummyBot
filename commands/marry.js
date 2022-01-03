7cc13e3ea5f373dcbf2ccf9453d5462f0b5b2129
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-02 16:08:13 -0800

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -40,7 +40,7 @@ module.exports = {
             message.awaitReactions({ filter, max: 1, time: (1000 * 10), errors: ['time'] })
                 .then(collected => {
                     const reaction = collected.first();
-
+                    console.log("A button was clicked");
                     if (reaction.emoji.name === '👍') {
                         message.reply('You reacted with a thumbs up.');
                     } else {
