f560a470e9b1460ea26a50413c3d07092176f72a
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-02 19:20:24 -0800

    fixed

--- gmcommands/save.js
+++ gmcommands/save.js
@@ -2,7 +2,7 @@ module.exports = {
     name: 'save',
     description: "Save files.",
 
-    execute(message, userData, userFish, userGarden, s3, userDataParams, userFishParams, userGardenParams) {
+    execute(message, userData, userFish, userGarden, config, s3, userDataParams, userFishParams, userGardenParams) {
         const { MessageEmbed } = require('discord.js');
         const embedMsg = new MessageEmbed();
 
