5c4e7695f6a0229bfd66419949e27a78f866cb9d
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-02 16:27:46 -0800

    Update marry.js

--- commands/marry.js
+++ commands/marry.js
@@ -34,20 +34,13 @@ module.exports = {
             message.react('👍').then(() => message.react('👎'));
 
             const filter = (reaction, user) => {
-                return ['👍', '👎'].includes(reaction.emoji.name);
+                return reaction.emoji.name === '👍' && user.id === message.author.id;
             };
-
-            message.awaitReactions({ filter, max: 1, time: (1000 * 10), errors: ['time'] })
-                .then(collected => {
-                    const reaction = collected.first();
-                    if (reaction.emoji.name == '👍') {
-                        message.reply('You reacted with a thumbs up.');
-                    } else {
-                        message.reply('You reacted with a thumbs down.');
-                    }
-                })
+            
+            message.awaitReactions({ filter, max: 4, time: 60000, errors: ['time'] })
+                .then(collected => console.log(collected.size))
                 .catch(collected => {
-                    message.reply('You reacted with neither a thumbs up, nor a thumbs down.');
+                    console.log(`After a minute, only ${collected.size} out of 4 reacted.`);
                 });
         }
         else {
