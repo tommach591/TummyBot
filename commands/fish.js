b4c573c56ab89a1684c16679945b2eed93137900
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-03 17:38:28 -0800

    Update fish.js

--- commands/fish.js
+++ commands/fish.js
@@ -629,6 +629,8 @@ module.exports = {
                         pages.push(page);
                     }
 
+                    const pagination = require('discord.js-pagination');
+
                     const emoji = ["⏪", "⏩"]
                     const timeout = '180000'
 
