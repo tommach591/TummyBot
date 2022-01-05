e6097b599b20cf1cf1213f4ac6799f40ef7b981f
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-01-05 01:10:44 -0800

    Update bj.js

--- commands/bj.js
+++ commands/bj.js
@@ -436,7 +436,7 @@ module.exports = {
                         }
                     }
                     else {
-                        embedMsg.setTitle('Invalid blackjack command or ammount!');
+                        embedMsg.setTitle('Invalid blackjack command or amount!');
                         embedMsg.setColor('FF0000');
                         embedMsg.setDescription("Use __!tp bj help__ for commands!");
                         embedMsg.setThumbnail("https://4.bp.blogspot.com/-DV8zj3oNPO8/XZKl8Y1_KkI/AAAAAAAMsvI/HEq47t0TPmYhX0b2igMkkxbcPQPbUXR2gCLcBGAsYHQ/s1600/AS0005827_02.gif");
