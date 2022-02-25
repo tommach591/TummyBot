a0a7c99f2c5f92ce2b2e9a61035ba5b621678f9a
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-02-25 12:24:55 -0800

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -195,7 +195,7 @@ module.exports = {
                 var stats = "Max HP: " + maxHP.toString() + "\nAttack: " + attack.toString() + "\nMagic: " + magic.toString() + "\nDefense: " + defense.toString() + "\nSpeed: " + speed.toString() + "\n";
                 var currentCondition = "HP: " + userHunt[userid].currentHP + "\n";
 
-                currentCondition += "Resistance: " + (100 - (100 / (100 + (defense * 15)))).toFixed + "%\n";
+                currentCondition += "Resistance: " + (100 - (100 / (100 + (defense * 15)))).toFixed(2) + "%\n";
                 
                 var critChance = (100 * (speed / 100));
                 if (critChance > 100) {
