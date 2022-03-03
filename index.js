d6a19967544c0e2a72e50c6c651c78702b900da3
Tom Mach <tom.m.mach@gmail.com>
2022-03-02 17:08:54 -0800

    hide key

--- index.js
+++ index.js
@@ -67,8 +67,8 @@ const userPetParams = {
 };
 
 const s3 = new AWS.S3({
-    accessKeyId: config.accessKeyID,
-    secretAccessKey: config.secretAccessKey,
+    accessKeyId: process.env.ACCESS_KEY_ID,
+    secretAccessKey: process.env.SECRET_ACCESS_KEY,
     Bucket: config.bucket
 });
 
