cf08e818e98064f386a589e637bcfddebd92c87d
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-03-08 20:29:49 -0800

    Update index.js

--- index.js
+++ index.js
@@ -654,9 +654,8 @@ client.on('messageCreate', message => {
         console.log(err);
     }
 
-    if (newTime.getTime() - savefile.lastSave >= (1000 * 60 * 15)) {
+    if (newTime.getTime() - savefile.lastSave.getTime() >= (1000 * 60 * 15)) {
         client.gmcommands.get('save').execute(message, userData, userFish, userGarden, userHunt, items, userPet, config, savefile, s3, userDataParams, userFishParams, userGardenParams, userHuntParams, itemsParams, userPetParams);
-        savefile.lastSave = newTime.getTime();
     }
 });
 
