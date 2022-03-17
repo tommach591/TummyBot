865e5e432198d8158d19543e7e0f109e7b31e167
Tom Mach <tom.m.mach@gmail.com>
2022-03-17 13:17:10 -0700

    cleaned balance

--- commands/balance.js
+++ commands/balance.js
@@ -3,124 +3,83 @@ module.exports = {
     description: "Displays your or somebody's balance.",
     
     execute(message, args, userid, userData, userFish, userGarden, client) {
-        try {
-            const { MessageEmbed } = require('discord.js');
-            const embedMsg = new MessageEmbed();
+        const { MessageEmbed } = require('discord.js');
+        const embedMsg = new MessageEmbed();
 
-            if (args.length > 0) {
-                var mention = args[0];
-                if (mention.startsWith('<@') && mention.endsWith('>')) {
-                    mention = mention.slice(2, -1);
+        let sendMsg = (id) => {
+            var target = client.users.cache.get(id);
+            embedMsg.setTitle(userData[id].name);
+            embedMsg.setThumbnail(target.displayAvatarURL());
+            var tier = "";
+            switch (userData[id].income) {
+                case 1:
+                    tier = "Bronze";
+                    embedMsg.setColor('CD7F32');
+                    break;
+                case 2:
+                    tier = "Silver";
+                    embedMsg.setColor('C0C0C0');
+                    break;
+                case 3:
+                    tier = "Gold";
+                    embedMsg.setColor('FFD700');
+                    break;
+                case 4:
+                    tier = "Platinum";
+                    embedMsg.setColor('3BF2A0');
+                    break;
+                case 5:
+                    tier = "Diamond";
+                    embedMsg.setColor('2EFAFF');
+                    break;
+            }
+            embedMsg.setFields(
+                {name: "__Points:__  :moneybag: ⠀⠀⠀⠀", value: "" + userData[id].points + "\n", inline: true},
+                {name: "__Income:__  :money_with_wings: ⠀⠀⠀⠀⠀", value: "" + tier + "\n", inline: true},
+                {name: "__Fame:__  :sparkles: ⠀⠀⠀⠀", value: "" + userData[id].fame + "\n", inline: true},
+                {name: "__Fishdex Entries:__  :fish: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userFish[id].fishdex.length + "\n", inline: false},
+                {name: "__Gardendex Entries:__  :sunflower: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userGarden[id].gardendex.length + "\n", inline: false}
+            );
+
+            if (userData[id].married != "" && userData[userData[id].married]) {
+                embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", userData[userData[id].married].name + "\n", false);
+            }
+            else {
+                embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", "Nobody\n", false);
+            }
+
+            message.channel.send({ embeds: [embedMsg] });
+        }
+
+        if (args.length > 0) {
+            var mention = args[0];
+            if (mention.startsWith('<@') && mention.endsWith('>')) {
+                mention = mention.slice(2, -1);
             
-                    if (mention.startsWith('!')) {
-                        mention = mention.slice(1);
-                    }
+                if (mention.startsWith('!')) {
+                    mention = mention.slice(1);
+                }
             
-                    if (!userData[mention]) {
-                        embedMsg.setTitle('Error!');
-                        embedMsg.setColor('FF0000');
-                        embedMsg.setDescription('User does not exist!');
-                        message.channel.send({ embeds: [embedMsg] });
-                        return;
-                    }
-                    var target = client.users.cache.get(mention);
-                    embedMsg.setTitle(userData[mention].name);
-                    embedMsg.setThumbnail(target.displayAvatarURL());
-                    var tier = "";
-                    switch (userData[mention].income) {
-                        case 1:
-                            tier = "Bronze";
-                            embedMsg.setColor('CD7F32');
-                            break;
-                        case 2:
-                            tier = "Silver";
-                            embedMsg.setColor('C0C0C0');
-                            break;
-                        case 3:
-                            tier = "Gold";
-                            embedMsg.setColor('FFD700');
-                            break;
-                        case 4:
-                            tier = "Platinum";
-                            embedMsg.setColor('3BF2A0');
-                            break;
-                        case 5:
-                            tier = "Diamond";
-                            embedMsg.setColor('2EFAFF');
-                            break;
-                    }
-                    embedMsg.setFields(
-                        {name: "__Points:__  :moneybag: ⠀⠀⠀⠀", value: "" + userData[mention].points + "\n", inline: true},
-                        {name: "__Income:__  :money_with_wings: ⠀⠀⠀⠀⠀", value: "" + tier + "\n", inline: true},
-                        {name: "__Fame:__  :sparkles: ⠀⠀⠀⠀", value: "" + userData[mention].fame + "\n", inline: true},
-                        {name: "__Fishdex Entries:__  :fish: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userFish[mention].fishdex.length + "\n", inline: false},
-                        {name: "__Gardendex Entries:__  :sunflower: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userGarden[mention].gardendex.length + "\n", inline: false}
-                    );
-
-                if (userData[mention].married != "" && userData[userData[mention].married]) {
-                    embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", userData[userData[mention].married].name + "\n", false);
-                }
-                else {
-                    embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", "Nobody\n", false);
-                }
-
-                    message.channel.send({ embeds: [embedMsg] });
-                }
-                else {
+                if (!userData[mention]) {
                     embedMsg.setTitle('Error!');
                     embedMsg.setColor('FF0000');
                     embedMsg.setDescription('User does not exist!');
                     message.channel.send({ embeds: [embedMsg] });
                     return;
                 }
+
+                endMsg(mention);
             }
             else {
-                embedMsg.setTitle(userData[userid].name);
-                var target = client.users.cache.get(userid);
-                embedMsg.setThumbnail(target.displayAvatarURL());
-                var tier = "";
-                switch (userData[userid].income) {
-                    case 1:
-                        tier = "Bronze";
-                        embedMsg.setColor('CD7F32');
-                        break;
-                    case 2:
-                        tier = "Silver";
-                        embedMsg.setColor('C0C0C0');
-                        break;
-                    case 3:
-                        tier = "Gold";
-                        embedMsg.setColor('FFD700');
-                        break;
-                    case 4:
-                        tier = "Platinum";
-                        embedMsg.setColor('3BF2A0');
-                        break;
-                    case 5:
-                        tier = "Diamond";
-                        embedMsg.setColor('2EFAFF');
-                        break;
-                }
-                embedMsg.setFields(
-                    {name: "__Points:__  :moneybag: ⠀⠀⠀⠀", value: "" + userData[userid].points + "\n", inline: true},
-                    {name: "__Income:__  :money_with_wings: ⠀⠀⠀⠀⠀", value: "" + tier + "\n", inline: true},
-                    {name: "__Fame:__  :sparkles: ⠀⠀⠀⠀", value: "" + userData[userid].fame + "\n", inline: true},
-                    {name: "__Fishdex Entries:__  :fish: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userFish[userid].fishdex.length + "\n", inline: false},
-                    {name: "__Gardendex Entries:__  :sunflower: ⠀⠀⠀⠀⠀⠀⠀", value: "" + userGarden[userid].gardendex.length + "\n", inline: false}
-                );
-
-                if (userData[userid].married != "" && userData[userData[userid].married]) {
-                    embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", userData[userData[userid].married].name + "\n", false);
-                }
-                else {
-                    embedMsg.addField("__Married To:__  :ring: ⠀⠀⠀⠀⠀⠀⠀", "Nobody\n", false);
-                }
-
+                embedMsg.setTitle('Error!');
+                embedMsg.setColor('FF0000');
+                embedMsg.setDescription('User does not exist!');
                 message.channel.send({ embeds: [embedMsg] });
+                return;
             }
         }
-        catch (err) {
-            throw err;
+        else {
+            endMsg(userid);
         }
     }
 }
