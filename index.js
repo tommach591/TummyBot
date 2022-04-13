14bbe5ca3b919fff295290bee94db04277137f8f
Tom Mach <tom.m.mach@gmail.com>
2022-04-12 17:41:41 -0700

    adjustments

--- index.js
+++ index.js
@@ -634,7 +634,7 @@ client.on('messageCreate', message => {
             currHunt["active"].channels[0].send({ embeds: [embedMsg] });
         }
 
-        if (currHunt["active"] && newTime.getTime() - currHunt["active"].lastPlayerAttack >= 1000 * 60 * 5 && !currHunt["active"].retreated && currHunt["active"].currentHP > 0) {
+        if (currHunt["active"] && newTime.getTime() - currHunt["active"].lastPlayerAttack >= 1000 * 60 * 10 && !currHunt["active"].retreated && currHunt["active"].currentHP > 0) {
             const embedMsg = new MessageEmbed();
             currHunt["active"].retreated = true;
             currHunt.lastSpawn = newTime.getTime();
