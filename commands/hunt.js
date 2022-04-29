579201c616b80a29ee7bfa3810790d47c5e2167b
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-04-29 12:08:15 -0700

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -54,6 +54,7 @@ module.exports = {
                                                                                                     // Mao vs Monks
             "https://i.gifer.com/ntL.gif",                                                          // Mugen vs Samurai
             "https://pa1.narvii.com/6792/1b835eb6198553d5c0a5528fccebea9290d72654_hq.gif",          // GGO Alt Bunny Pew Pew
+            "https://c.tenor.com/LlZtlf4W7C8AAAAC/bts-suga.gif"                                     // PSY Slap Suga
         ];
 
         let generateEquip = (itemName) => {
