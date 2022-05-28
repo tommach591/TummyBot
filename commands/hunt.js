d58e95f12f4346267a94f64798be95c3d6062611
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-05-27 18:03:33 -0700

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -2408,7 +2408,7 @@ module.exports = {
                 break;
             case 'alert':
                 let role = message.guild.roles.cache.find(role => role.name === "guild");
-                var player = message.guild.members;
+                var player = message.guild.members.fetch(userid);
                 
                 console.log(player);
                 console.log(role);
