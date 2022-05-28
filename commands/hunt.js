8e9adaeab21e1cce85b31a32bc45bdfc27122ffb
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-05-27 18:40:30 -0700

    Update hunt.js

--- commands/hunt.js
+++ commands/hunt.js
@@ -2408,60 +2408,61 @@ module.exports = {
                 break;
             case 'alert':
                 let role = message.guild.roles.cache.find(role => role.name === "guild");
-                var player = client.users.cache.get(userid);
-                
-                console.log(player);
-                console.log(role);
+                var player = message.guild.members.fetch(userid).then(() => 
+                {
+                    console.log(player);
+                    console.log(role);
 
-                var choice = args[1];
-                if (args.length < 2 || (choice != "on" && choice != "off"))
-                {
-                    embedMsg.setTitle('Error');
-                    embedMsg.setColor('FF0000');
-                    embedMsg.setDescription('Tell the bot you want it turned on or off!');
-                    embedMsg.setFooter('!tp alert on/off')
-                    message.channel.send({ embeds: [embedMsg] });
-                    break;
-                }
-                else
-                {
-                    if (choice == "on")
+                    var choice = args[1];
+                    if (args.length < 2 || (choice != "on" && choice != "off"))
                     {
-                        if (!player.roles.includes(role.id))
+                        embedMsg.setTitle('Error');
+                        embedMsg.setColor('FF0000');
+                        embedMsg.setDescription('Tell the bot you want it turned on or off!');
+                        embedMsg.setFooter('!tp alert on/off')
+                        message.channel.send({ embeds: [embedMsg] });
+
+                    }
+                    else
+                    {
+                        if (choice == "on")
                         {
-                            player.roles.add(role)
-                            embedMsg.setTitle('Success');
-                            embedMsg.setColor('00FF00');
-                            embedMsg.setDescription(masterData["userData"][userid].name + ' will be alerted for bosses!');
-                            message.channel.send({ embeds: [embedMsg] });
+                            if (!player.roles.includes(role.id))
+                            {
+                                player.roles.add(role)
+                                embedMsg.setTitle('Success');
+                                embedMsg.setColor('00FF00');
+                                embedMsg.setDescription(masterData["userData"][userid].name + ' will be alerted for bosses!');
+                                message.channel.send({ embeds: [embedMsg] });
+                            }
+                            else
+                            {
+                                embedMsg.setTitle('Error');
+                                embedMsg.setColor('FF0000');
+                                embedMsg.setDescription(masterData["userData"][userid].name + ' is already signed up for boss alerts!');
+                                message.channel.send({ embeds: [embedMsg] });
+                            }
                         }
-                        else
+                        else if (choice == "off")
                         {
-                            embedMsg.setTitle('Error');
-                            embedMsg.setColor('FF0000');
-                            embedMsg.setDescription(masterData["userData"][userid].name + ' is already signed up for boss alerts!');
-                            message.channel.send({ embeds: [embedMsg] });
+                            if (player.roles.includes(role.id))
+                            {
+                                player.roles.remove(role.id);
+                                embedMsg.setTitle('Success');
+                                embedMsg.setColor('00FF00');
+                                embedMsg.setDescription(masterData["userData"][userid].name + ' will not be alerted for bosses!');
+                                message.channel.send({ embeds: [embedMsg] });
+                            }
+                            else
+                            {
+                                embedMsg.setTitle('Error');
+                                embedMsg.setColor('FF0000');
+                                embedMsg.setDescription(masterData["userData"][userid].name + ' is not signed up for boss alerts!');
+                                message.channel.send({ embeds: [embedMsg] });
+                            }
                         }
                     }
-                    else if (choice == "off")
-                    {
-                        if (player.roles.includes(role.id))
-                        {
-                            player.roles.remove(role.id);
-                            embedMsg.setTitle('Success');
-                            embedMsg.setColor('00FF00');
-                            embedMsg.setDescription(masterData["userData"][userid].name + ' will not be alerted for bosses!');
-                            message.channel.send({ embeds: [embedMsg] });
-                        }
-                        else
-                        {
-                            embedMsg.setTitle('Error');
-                            embedMsg.setColor('FF0000');
-                            embedMsg.setDescription(masterData["userData"][userid].name + ' is not signed up for boss alerts!');
-                            message.channel.send({ embeds: [embedMsg] });
-                        }
-                    }
-                }
+                });
                 break;
             default:
                 embedMsg.setTitle('Invalid hunting command!');
