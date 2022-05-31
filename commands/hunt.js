b05eb71cdfdb60ecd5d44c4721950ba67a7ec6b2
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-05-31 00:04:40 -0700

    a

--- commands/hunt.js
+++ commands/hunt.js
@@ -616,7 +616,7 @@ module.exports = {
                         
                         var isCrit = false;
 
-                        var crit = Math.floor((Math.random() * 101));
+                        var crit = Math.floor((Math.random() * 100)) + 1;
 
                         if (crit <= critChance) {
                             isCrit = true;
