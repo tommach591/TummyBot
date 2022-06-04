dca697f24fce2fbe2b0e1369b81b60dba5648c86
Tom Mach <57111473+tommach591@users.noreply.github.com>
2022-06-03 23:33:35 -0700

    Update beg.js

--- commands/beg.js
+++ commands/beg.js
@@ -13,13 +13,14 @@ module.exports = {
         var begCD = 1000 * 60 * 10; // 10min
 
         var begTime = 5000;
-        beggingMsg.setTitle('The Beggar Prays!');
-        beggingMsg.setDescription('Begging in progress...');
-        beggingMsg.setImage('https://thumbs.gfycat.com/MiserableAmusedHarvestmen-max-1mb.gif');
-        message.channel.send({ embeds: [beggingMsg] }).then(msg=> {setTimeout(() => msg.delete(), begTime - 500)});
 
-        setTimeout(function() { 
-            if (timeDiff >= begCD) {
+        if (timeDiff >= begCD) {
+            beggingMsg.setTitle('The Beggar Prays!');
+            beggingMsg.setDescription('Begging in progress...');
+            beggingMsg.setImage('https://thumbs.gfycat.com/MiserableAmusedHarvestmen-max-1mb.gif');
+            message.channel.send({ embeds: [beggingMsg] }).then(msg=> {setTimeout(() => msg.delete(), begTime - 500)});
+            
+            setTimeout(function() { 
                 var pity = Math.floor(Math.random() * 49) + 1;
                 masterData["userData"][userid].points += pity;
                 embedMsg.setColor('00FF00');
@@ -27,13 +28,13 @@ module.exports = {
                 embedMsg.setImage('http://media1.giphy.com/media/Sb9g8EwGfrPqg/giphy.gif');
                 message.channel.send({ embeds: [embedMsg] });
                 masterData["userData"][userid].begTime = newTime.getTime();
-            }
-            else {
-                embedMsg.setColor('FF0000');
-                embedMsg.setDescription("The gods are busy drinking tea for another " + Math.floor((begCD - timeDiff) / 1000) + " seconds.");
-                embedMsg.setImage('https://c.tenor.com/FhBDy-T1L6AAAAAM/kermit-the-frog-sip.gif');
-                message.channel.send({ embeds: [embedMsg] });
-            }
-        }, begTime);
+            }, begTime);
+        }
+        else {
+            embedMsg.setColor('FF0000');
+            embedMsg.setDescription("The gods are busy drinking tea for another " + Math.floor((begCD - timeDiff) / 1000) + " seconds.");
+            embedMsg.setImage('https://c.tenor.com/FhBDy-T1L6AAAAAM/kermit-the-frog-sip.gif');
+            message.channel.send({ embeds: [embedMsg] });
+        }
     }
 }
